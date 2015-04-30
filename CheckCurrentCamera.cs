080b40074d2600273cf709244c6696cb7ce99548
Graham Reeves <graham@grahamreeves.com>
2015-04-30 16:23:38 +0100

    Ensure Camera.current is set, which can cause crashes in unity 5.0.1f1

---
+++ CheckCurrentCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+	There's a bug in unity 5.0.1f1 where using Graphics.Blit() crashes in Update()(where I NEED to do blits) if there is no current camera set.
+	Possibly due to specific texture types...
+	Call this beforehand.
+*/
+public class CheckCurrentCamera : MonoBehaviour {
+
+	static public bool CheckCurrentCamera()
+	{
+		if (!Camera.current)
+			Camera.SetupCurrent (Camera.main);
+		return Camera.current != null;
+	}
+}
