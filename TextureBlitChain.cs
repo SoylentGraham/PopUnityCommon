c4662193b2d690daffe6cc8672a8dd6f657042fe
Graham Reeves <graham@grahamreeves.com>
2015-06-30 17:20:28 +0100

    Added texture shader-blit chain

---
+++ TextureBlitChain.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextureBlitChain : MonoBehaviour {
+
+	public List<Material>		mShaders;
+
+	public void Execute (RenderTexture mInput,RenderTexture mOutput) 
+	{
+		if (!mInput || !mOutput)
+			return;
+
+		RenderTexture BufferIn = RenderTexture.GetTemporary (mInput.width, mInput.height, mInput.depth, mInput.format);
+		RenderTexture BufferOut = RenderTexture.GetTemporary (mInput.width, mInput.height, mInput.depth, mInput.format);
+		Graphics.Blit (mInput, BufferOut);
+
+		for (int i=0; i<mShaders.Count; i++) {
+			//	last output, is now input
+			RenderTexture Swap = BufferOut;
+			BufferOut = BufferIn;
+			BufferIn = Swap;
+
+			if( mShaders[i] != null )
+				Graphics.Blit (BufferIn, BufferOut, mShaders [i]);
+			else
+				Graphics.Blit (BufferIn, BufferOut);
+		}
+
+		//	output
+		Graphics.Blit (BufferOut, mOutput);
+
+		RenderTexture.ReleaseTemporary (BufferIn);
+		RenderTexture.ReleaseTemporary (BufferOut);
+
+	}
+}
