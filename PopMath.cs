f115599de1cbcdbe8c11d11178a7498c2e38561b
Graham Reeves <graham@grahamreeves.com>
2015-06-30 17:22:08 +0100

    Added a collection of some simple math functionality (most rect based atm)

---
+++ PopMath.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopMath {
+
+	public static Rect RectToScreen(Rect RectNorm)
+	{
+		return RectMult (RectNorm, new Rect (0, 0, Screen.width, Screen.height));
+	}
+	
+	public static Rect RectMult(Rect RectNorm,Rect Body)
+	{
+		RectNorm.x *= Body.width;
+		RectNorm.width *= Body.width;
+		RectNorm.y *= Body.height;
+		RectNorm.height *= Body.height;
+		RectNorm.x += Body.x;
+		RectNorm.y += Body.y;
+		return RectNorm;
+	}
+
+
+	public static Rect GetTextureRect(Texture Tex)
+	{
+		if ( !Tex )
+			return new Rect(0,0,0,0);
+
+		return new Rect( 0, 0, Tex.width, Tex.height );
+	}
+
+	public static Rect Round(Rect r)
+	{
+		r.x = Mathf.Round (r.x);
+		r.y = Mathf.Round (r.y);
+		r.width = Mathf.Round (r.width);
+		r.height = Mathf.Round (r.height);
+		return r;
+	}
+
+	public static Rect ViewRectToTextureRect(Rect ViewRect,Texture Tex)
+	{
+		return new Rect (ViewRect.x, Tex.height-ViewRect.yMax, ViewRect.width, ViewRect.height);
+	}
+
+}
+
