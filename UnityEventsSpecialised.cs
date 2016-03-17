fedf94146e4859a2f2b4b31068dccf6054207233
Graham Reeves <graham@grahamreeves.com>
2016-03-17 13:45:14 +0000

    Added some UnityEvent specialisations

---
+++ UnityEventsSpecialised.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+
+//	gr: I had a script in PopUnityCommon somewhere for this...
+[System.Serializable]
+public class UnityEvent_String : UnityEvent <string> {}
+
+
