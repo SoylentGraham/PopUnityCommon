6d0b40fed16b9f90f84e887247a538fc4126f504
Graham Reeves <graham@grahamreeves.com>
2016-05-11 14:11:13 +0100

    Added editor window which renders a camera of your choice, handy for testing multi-display scenes

---
+++ CameraPreviewWindow.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class CameraPreviewWindow : UnityEditor.EditorWindow
+{
+	public Camera mCamera;
+	public RenderTexture mRenderTexture;
+
+	// Add menu named "My Window" to the Window menu
+	[MenuItem ("Camera Preview Window/Create Camera Preview Window")]
+	static void Init () 
+	{
+		//	create a new window
+		var NewWindow  = EditorWindow.CreateInstance<CameraPreviewWindow>();
+		NewWindow.ShowUtility();
+	}
+
+	void UpdateRenderTexture(Rect WindowSize)
+	{
+		//	render camera
+		if (mCamera == null) {
+			mRenderTexture = null;
+		} else {
+			if (mRenderTexture != null && (mRenderTexture.width != WindowSize.width || mRenderTexture.height != WindowSize.height)) {
+				mRenderTexture = null;
+			}
+
+			if (mRenderTexture == null) {
+				int CameraRenderTextureDepth = (mCamera.targetTexture != null) ? mCamera.targetTexture.depth : 24;
+				mRenderTexture = new RenderTexture ( (int)WindowSize.width, (int)WindowSize.height, CameraRenderTextureDepth);
+			}
+
+			var OldTarget = mCamera.targetTexture;
+			mCamera.targetTexture = mRenderTexture;
+			mCamera.Render ();
+			mCamera.targetTexture = OldTarget;
+
+		}
+	}
+
+	void SnapWindowToDisplay()
+	{
+		if ( mCamera == null )
+			return;
+
+		//	get disp
+	}
+
+	void OnGUI()
+	{
+		var Rect = this.position;
+		Rect.x = 0;
+		Rect.y = 0;
+			
+		//	draw camera selection
+		{
+			int SelectedCamera = 0;
+			List<string> CameraNames = new List<string> ();
+			CameraNames.Add ("null");
+
+			//	add the other cameras in the scene
+			foreach (Camera Cam in Camera.allCameras) {
+				CameraNames.Add (Cam.name);
+				if (mCamera == Cam)
+					SelectedCamera = CameraNames.Count - 1;
+			}
+
+			int NewSelectedCamera = EditorGUILayout.Popup ( SelectedCamera, CameraNames.ToArray(), GUILayout.ExpandWidth (true));
+
+			//	changed camera
+			if (NewSelectedCamera != SelectedCamera) {
+				if (NewSelectedCamera == 0) {
+					mCamera = null;
+				} else {
+					mCamera = Camera.allCameras [NewSelectedCamera -1];
+				}
+			}
+		}
+
+		{
+			if (GUILayout.Button ("Snap to display")) {
+				SnapWindowToDisplay ();
+			}
+		}
+
+
+		//	draw the content!
+		{
+			//	move the rect to go past all the previous controls
+			var LastBottom = GUILayoutUtility.GetLastRect ().yMax;
+			Rect.y += LastBottom;
+			Rect.height -= LastBottom;
+
+			UpdateRenderTexture (Rect);
+			if (mRenderTexture == null) {
+				GUI.Box (Rect, "No camera");
+			} else {
+				GUI.DrawTexture (Rect, mRenderTexture);
+			}
+		}
+	}
+}
+
