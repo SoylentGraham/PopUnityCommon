07a3e6f4625d290f72bfc59796fbfbcdda28c296
Graham Reeves <graham@grahamreeves.com>
2016-11-20 21:50:55 +0000

    Added simple script to blit one texture to another with a shader

---
+++ ShaderBlit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+[ExecuteInEditMode]
+public class ShaderBlit : MonoBehaviour {
+
+	public bool					Dirty = true;
+	public Texture				Input;
+	public Shader				BlitShader;
+	public Material				BlitMaterial;
+	public RenderTexture		Output;
+	public UnityEngine.Events.UnityEvent	OnClean;
+
+	public void SetDirty()
+	{ 
+		Dirty = true;
+	}
+
+	void Update ()
+	{
+		if ( !Dirty )
+			return;
+
+		if ( Input == null )
+			return;
+
+		if (BlitShader != null)
+		{
+			if ( BlitMaterial == null )
+				BlitMaterial = new Material( BlitShader );
+		}
+
+		if ( BlitMaterial == null )
+			return;
+
+		Graphics.Blit( Input, Output, BlitMaterial );
+
+		Dirty = false;
+
+		if ( OnClean != null )
+			OnClean.Invoke();
+
+	}
+}
