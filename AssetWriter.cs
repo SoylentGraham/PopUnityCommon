f57282947cd642cb344a1583b8a420c4f22ce1b7
Graham Reeves <graham@grahamreeves.com>
2017-01-18 12:02:19 +0000

    Added a quick asset writer wrapper that creates a new, or overwrites an existing asset

---
+++ AssetWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetWriter : MonoBehaviour {
+
+	#if UNITY_EDITOR
+	public static T WriteAsset<T>(string Name,T Asset) where T : Object
+	{
+		if (Asset == null)
+			return null;
+
+		var Path = "Assets/" + Name + ".asset";
+
+		var ExistingAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(Path);
+
+		if ( ExistingAsset == null )
+		{
+			UnityEditor.AssetDatabase.CreateAsset( Asset, Path );
+		}
+		else
+		{
+			UnityEditor.EditorUtility.CopySerialized(Asset, ExistingAsset);
+		}
+
+		return UnityEditor.AssetDatabase.LoadAssetAtPath<T>(Path);
+	}
+	#endif
+}
