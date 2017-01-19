d1f5d324385f0add6977bcd6cc1ee8661ba7e6e8
Graham Reeves <graham@grahamreeves.com>
2017-01-19 19:49:08 +0000

    Added a very quick script to modify a mesh's bounds to fit your collider

---
+++ SetMeshBoundsToCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMeshBoundsToCollider : MonoBehaviour {
+
+	void Start () {
+	
+		var Mesh = GetComponent<MeshFilter> ().mesh;
+		var Collider = GetComponent<Collider> ();
+		if (Collider && Mesh) {
+			Mesh.bounds = Collider.bounds;
+		}
+
+	}
+}
+
+
