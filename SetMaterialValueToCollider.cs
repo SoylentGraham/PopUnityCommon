922983ccdea9335686a80914e1502860302c605a
Graham Reeves <graham@grahamreeves.com>
2017-01-19 19:09:06 +0000

    Added component to go with SetMaterialValue to push collider shape to material

---
+++ SetMaterialValueToCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class UnityEvent_Vector4 : UnityEngine.Events.UnityEvent <Vector4> {}
+
+
+
+[ExecuteInEditMode]
+public class SetMaterialValueToCollider : MonoBehaviour {
+
+	public UnityEvent_Vector4	OnColliderChanged;
+
+	void Start () {
+
+		var Sphere = GetComponent<SphereCollider> ();
+		if (Sphere) {
+			var Sphere4 = new Vector4 (Sphere.center.x, Sphere.center.y, Sphere.center.z, Sphere.radius);
+			OnColliderChanged.Invoke (Sphere4);
+		}
+	}
+
+	#if UNITY_EDITOR
+	void Update()
+	{
+		Start();
+	}
+	#endif
+	
+}
