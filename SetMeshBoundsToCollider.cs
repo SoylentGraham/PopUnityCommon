6153dea5f2e7b90a5c8eb7676ef5c14910f70c56
Graham Reeves <graham@grahamreeves.com>
2017-01-23 12:25:38 +0000

    SetMeshBoundsToCollider is a bit more flexible now

--- SetMeshBoundsToCollider.cs
+++ SetMeshBoundsToCollider.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class SetMeshBoundsToCollider : MonoBehaviour {
 
+
+	[InspectorButton("SetMeshFilterMeshBoundsToCollider")]
+	public bool _SetMeshFilterMeshBoundsToCollider;
+
+
 	void Start () {
 	
-		var Mesh = GetComponent<MeshFilter> ().mesh;
+		SetMeshFilterMeshBoundsToCollider ();
+	}
+
+
+	public void SetMeshFilterMeshBoundsToCollider()
+	{
+		var mf = GetComponent<MeshFilter> ();
+		if (mf) {
+			var Mesh = mf.mesh;
+			SetMeshBoundsToMeshFilterMesh (Mesh);
+		}
+
+	}
+
+	public void SetMeshBoundsToMeshFilterMesh(Mesh mesh)
+	{
 		var Collider = GetComponent<Collider> ();
-		if (Collider && Mesh) {
-			Mesh.bounds = Collider.bounds;
+		if (Collider && mesh) {
+			mesh.bounds = Collider.bounds;
 		}
 
 	}
