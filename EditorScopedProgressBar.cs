62be098be9bce37a2c236c1a5d76ac2897fd63f4
Graham Reeves <graham@grahamreeves.com>
2017-04-10 17:25:39 +0100

    Added a scoped (via disposable/using) progress bar

---
+++ EditorScopedProgressBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/*
+To make this go out of scope without having to call YourProgress.Dispose() or GC.Collect, use the Using keyword
+
+	using (var Progress = new ScopedProgressBar())
+	{
+	     ...
+	}
+*/
+public class ScopedProgressBar : System.IDisposable
+{
+	string	mTitle;
+
+	public ScopedProgressBar(string Title)
+	{
+		mTitle = Title;
+		#if UNITY_EDITOR
+		EditorUtility.DisplayProgressBar (mTitle, "...", 0.0f);
+		#endif
+	}
+
+	~ScopedProgressBar()
+	{
+		//	can't use destructor, not on the unity main thread. have to use disposable interface.
+		//Dispose ();
+	}
+
+	public void Dispose()
+	{
+		#if UNITY_EDITOR
+		EditorUtility.ClearProgressBar();
+		#endif
+	}
+
+	public void SetProgress(string StepName,float Progress)
+	{
+		#if UNITY_EDITOR
+		if (EditorUtility.DisplayCancelableProgressBar (mTitle, StepName, Progress))
+			throw new System.Exception (mTitle + " cancelled");
+		#endif
+	}
+};
+
