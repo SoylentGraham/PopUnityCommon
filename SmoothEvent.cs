1ba5b5186608fd6c22815dc0ed40fa24b428657a
Graham Reeves <graham@grahamreeves.com>
2017-07-09 21:35:55 +0100

    Added OnEnabled component for simple triggering. SmoothEvent for time-delay events

---
+++ SmoothEvent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SmoothEvent : MonoBehaviour {
+
+	[Header("Called every frame")]
+	public UnityEvent_float		OnUpdate;
+	public UnityEvent_float		OnUpdateInverse;
+
+	public UnityEvent			OnFinished;
+	public bool					DisableOnFinished = true;
+
+	[Header("Curve will be stretched to this duration")]
+	[Range(0.01f,20.0f)]
+	public float				Duration = 5;
+	public AnimationCurve		Curve;
+
+
+	float				StartTime = 0;
+
+
+	void OnEnable()
+	{
+		StartTime = Time.time;
+		Update ();
+	}		
+
+	void Update () {
+
+		float TimePassed = Time.time - StartTime;
+
+
+		if (TimePassed > Duration) {
+			OnFinished.Invoke ();
+			TimePassed = Duration;
+
+			if (DisableOnFinished)
+				this.enabled = false;
+		}
+
+		float Value = 0;
+		if (Curve.keys.Length > 1) {
+			var t = TimePassed / Duration;
+			var CurveDuration = Curve.keys [Curve.length - 1].time;
+			t = t * CurveDuration;
+			Value = Curve.Evaluate (t);
+		} else {
+			Value = TimePassed / Duration;
+		}
+
+		OnUpdate.Invoke (Value);
+		OnUpdateInverse.Invoke (1 - Value);
+	
+	}
+}
