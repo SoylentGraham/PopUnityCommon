94a4d65095d3b6f0db56c5ac9fddd66d93bf179c
Graham Reeves <graham@grahamreeves.com>
2017-08-10 22:24:31 +0100

    Added menu option to restart the editor in opengl

---
+++ PopRestartEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PopRestartEditor : MonoBehaviour {
+
+	static string	ArgForceOpengl = "-force-opengl";
+
+#if UNITY_EDITOR
+	[MenuItem("NewChromantics/Restart Editor in opengl")]
+	static void RestartEditorInOpengl()
+	{
+		//var UnityExe = System.Environment.GetCommandLineArgs()[0];
+		//var UnityExe = EditorApplication.applicationPath;
+		//System.Diagnostics.Process.Start( UnityExe, ArgForceOpengl );
+		EditorApplication.OpenProject("./", ArgForceOpengl);
+	}
+#endif
+}
+
