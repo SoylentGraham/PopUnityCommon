4bdef48b66c7d190c5221af2fa4bfddbc4679eea
Graham Reeves <graham@grahamreeves.com>
2017-08-28 16:58:02 +0100

    Added script to show/modify steamvr configuration file

---
+++ SteamvrSettings.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SteamVrSettings : MonoBehaviour
+{
+
+	const string SettingsFilename = "default.vrsettings";
+	const string RequireHmdKey = "requireHmd";
+
+	//	todo, work this out from env vars etc
+	const string SettingsFolder = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR\\resources\\settings\\";
+
+	static string SettingsPath	{	get	{	return SettingsFolder + SettingsFilename;	}	}
+
+#if UNITY_EDITOR
+	[MenuItem("SteamVR/Set RequireHmd False")]
+	public static void Set_RequireHmd_False()
+	{
+		SetVrSettings( RequireHmdKey, "false" );
+	}
+#endif
+
+#if UNITY_EDITOR
+	[MenuItem("SteamVR/Set RequireHmd True")]
+	public static void Set_RequireHmd_True()
+	{
+		SetVrSettings( RequireHmdKey, "true" );
+	}
+#endif
+	
+#if UNITY_EDITOR
+	[MenuItem("SteamVR/Show " + SettingsFilename)]
+	public static void ShowSettingsFile()
+	{
+		EditorUtility.RevealInFinder( SettingsPath );
+	}
+#endif
+
+#if UNITY_EDITOR
+	public static void SetVrSettings(string Key,string NewValue)
+	{
+
+		//	setup regular expression
+		string EscapedKey =  "\"" + Key + "\" : ";
+		string MatchPattern = EscapedKey + "(true|false),";
+		//	gr: I thought it would just repalce the match, but in c# the whole match is replaced :)
+		//string Replacement = NewCategory;
+		string Replacement = EscapedKey + NewValue + ",";
+		Regex RegExpression = new Regex(MatchPattern);
+
+		string NoChangeFilenames = null;
+
+		var FilePaths = new string[1] { SettingsPath };
+
+		foreach (var Path in FilePaths) {
+			try
+			{
+				var Contents = System.IO.File.ReadAllText( Path );
+				var NewContents = RegExpression.Replace( Contents, Replacement );
+				if ( Contents != NewContents )
+				{
+					System.IO.File.WriteAllText( Path, NewContents );
+					Debug.Log("Updated " + Path );
+				}
+				else
+				{
+					NoChangeFilenames += Path + " ";
+					//Debug.Log("skipped " + Path );
+				}
+			}
+			catch(System.Exception e) {
+				Debug.LogError ("Error replacing " + Key + " in " + Path);
+				Debug.LogException (e);
+			}
+		}
+
+		if (!string.IsNullOrEmpty (NoChangeFilenames))
+			Debug.Log ("Didn't change " + NoChangeFilenames);
+
+	}
+#endif
+
+
+}
