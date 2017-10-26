93a8dc5da258150b042362c6eee194b795d87f0d
Graham Reeves <graham@newchromantics.com>
2017-10-26 16:54:19 +0100

    Added PopX.IO namespace for file utility functions

---
+++ PopIO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//	gr: PopX being renamed to Pop later
+namespace PopX
+{
+	public static class IO
+	{
+		public static void WriteStringToFile (string Filename, string Data)
+		{
+			var FileHandle = System.IO.File.CreateText( Filename );
+			FileHandle.Write(Data);
+			FileHandle.Close();
+		}
+	}
+}
+
