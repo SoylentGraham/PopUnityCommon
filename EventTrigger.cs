cf88756fd5fe8c1c189c6e0de1ee134ac59dc75f
Graham Reeves <graham@newchromantics.com>
2017-10-31 21:05:44 +0000

    Added EventTrigger and RelayFloatValue for animating arbritry values

---
+++ EventTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+
+    [Header("Normal triggers")]
+	public UnityEvent OnTriggered1;
+	public UnityEvent OnTriggered2;
+	public UnityEvent OnTriggered3;
+	public UnityEvent OnTriggered4;
+	public UnityEvent OnTriggered5;
+
+    public void Trigger1()
+	{
+		OnTriggered1.Invoke();
+	}
+	public void Trigger2()
+	{
+		OnTriggered2.Invoke();
+	}
+	public void Trigger3()
+	{
+		OnTriggered3.Invoke();
+    }
+    public void Trigger4()
+    {
+		OnTriggered4.Invoke();
+    }
+    public void Trigger5()
+    {
+		OnTriggered5.Invoke();
+    }
+
+
+}
---
+++ RelayFloatValue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class RelayFloatValue : MonoBehaviour
+{
+
+	public bool SetDefaultOnStart = true;
+	public bool SetDefaultOnDestroy = true;
+	public float DefaultValue;
+    public float Value;
+    public UnityEvent_float OnUpdate;
+
+    void Start()
+    {
+		if ( SetDefaultOnStart )
+	        OnUpdate.Invoke(DefaultValue);
+    }
+
+    void Update()
+    {
+        OnUpdate.Invoke(Value);
+    }
+
+    void OnDestroy()
+    {
+		if ( SetDefaultOnDestroy )
+	        OnUpdate.Invoke(DefaultValue);
+    }
+}
