e3526d45b1d3914a7e65c1058a828fffd1b4b333
Graham Reeves <graham@newchromantics.com>
2017-11-01 14:00:03 +0000

    Script that can change the time of a timeline

---
+++ SetTimelineTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class SetTimelineTime : MonoBehaviour {
+
+	PlayableDirector	TimelineDirector	{	get { return GetComponent<PlayableDirector> (); } }
+
+	public void SetTime(float Seconds)
+	{
+		var Timeline = TimelineDirector;
+		Timeline.time = Seconds;
+	}
+}
