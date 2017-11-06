ebdcd2f7b09f09d410a36273362fe0e72bda6d33
Graham Reeves <graham@newchromantics.com>
2017-11-06 15:47:47 +0000

    Added script that sends out an event with the current time on enable & on disable

---
+++ OnEnableTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnableTime : MonoBehaviour {
+
+	public UnityEvent_float	OnEnabledTime;
+	public UnityEvent_float	OnDisabledTime;
+
+	void OnEnable()
+	{
+		OnEnabledTime.Invoke (Time.time);
+	}
+
+	void OnDisable()
+	{
+		OnDisabledTime.Invoke (Time.time);
+	}
+}
