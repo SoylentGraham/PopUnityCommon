ddf3bfbc76127acfc0a0a18ccfbfa2777cda2755
Graham Reeves <graham@newchromantics.com>
2017-11-06 16:12:49 +0000

    Added option to report the timeSinceLevelLoad (which matches _Time in shaders)

--- OnEnableTime.cs
+++ OnEnableTime.cs
@@ -7,13 +7,17 @@ public class OnEnableTime : MonoBehaviour {
 	public UnityEvent_float	OnEnabledTime;
 	public UnityEvent_float	OnDisabledTime;
 
+	[Header("Use timeSinceLevelLoad to match shader _Time")]
+	//	https://answers.unity.com/questions/302335/built-in-shaderlab-timey-and-timetime-are-not-equa.html
+	public bool				UseTimeSinceLevelLoad = true;
+
 	void OnEnable()
 	{
-		OnEnabledTime.Invoke (Time.time);
+		OnEnabledTime.Invoke ( UseTimeSinceLevelLoad ? Time.timeSinceLevelLoad : Time.time );
 	}
 
 	void OnDisable()
 	{
-		OnDisabledTime.Invoke (Time.time);
+		OnDisabledTime.Invoke ( UseTimeSinceLevelLoad ? Time.timeSinceLevelLoad : Time.time );
 	}
 }
