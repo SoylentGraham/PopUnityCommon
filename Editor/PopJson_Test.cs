1cbcfea4cdee0771c3dfafd699f0ff41ec6eff97
Graham Reeves <graham@newchromantics.com>
2018-01-08 18:48:10 +0000

    Added (basic) string value escaping for json. Added Json.Append to insert into existing json. + Unit test

--- Editor/PopJson_Test.cs
+++ Editor/PopJson_Test.cs
@@ -10,23 +10,64 @@ namespace PopX
 	public class Json_UnitTest
 	{
 		[System.Serializable]
-		public class TestA
+		public class StructHello
 		{
-			public string	One;
-			public TestA(string _One)	{	One = _One;	}
+			public string	Hello;
+
+			public StructHello(string _HelloValue)	
+			{
+				Hello = _HelloValue;	
+			}
+		};
+
+		[System.Serializable]
+		public class StructHelloCat : StructHello
+		{
+			public string	Cat;
+
+			public StructHelloCat(string _HelloValue,string _CatValue) : base(_HelloValue)
+			{
+				Cat = _CatValue;	
+			}
 		};
 
 		[Test]
 		public void Replace_Test()
 		{
 			//	use a struct to avoid whitespace hassle. We just need to know if it de/serialises
-			var Struct = new TestA ("one");
-			var NewValue = "Hello";
+			var Struct = new StructHello ("World");
+			var NewValue = "Universe";
 			var Json = JsonUtility.ToJson (Struct);
-			PopX.Json.Replace (ref Json, "One", NewValue);
+			PopX.Json.Replace (ref Json, "Hello", NewValue);
 			JsonUtility.FromJsonOverwrite (Json, Struct);
 
-			Assert.AreEqual (Struct.One, NewValue);
+			Assert.AreEqual ( NewValue,Struct.Hello);
+		}
+
+		[Test]
+		public void Append_ToEmptyJson()
+		{
+			var Json = "{}";
+			var Value = "World";
+			PopX.Json.Append (ref Json, "Hello",Value);
+
+			//	read new json (will throw json syntax errors for us)
+			var Struct = JsonUtility.FromJson<StructHello>(Json);
+
+			Assert.AreEqual (Value,Struct.Hello);
+		}
+
+		[Test]
+		public void Append_ToShallowJson()
+		{
+			var Json = JsonUtility.ToJson (new StructHello ("World"));
+			var CatValue = "Dog";
+			PopX.Json.Append (ref Json, "Cat", CatValue );
+
+			//	read new json (will throw json syntax errors for us)
+			var Struct = JsonUtility.FromJson<StructHelloCat>(Json);
+
+			Assert.AreEqual (CatValue,Struct.Cat);
 		}
 	}
 }
--- PopJson.cs
+++ PopJson.cs
@@ -60,18 +60,96 @@ namespace PopX
 			return GetJsonLength((i) => { return (char)Data[i]; });
 		}
 
+		static void EscapeChar(ref char[] CharPair)
+		{
+			switch(CharPair[0])
+			{
+			case '\n':	CharPair[0] = '\\';	CharPair[1] = 'n';	break;
+			case '\t':	CharPair[0] = '\\';	CharPair[1] = 't';	break;
+			case '\r':	CharPair[0] = '\\';	CharPair[1] = 'r';	break;
+			case '\\':	CharPair[0] = '\\';	CharPair[1] = '\\';	break;
+			}
+		}
+
+		static public string EscapeValue(string Unescaped,bool WrapInQuotes=true)
+		{
+			var Escaped = WrapInQuotes ? "\"" : "";
+
+			var CharPair = new char[2];
+			var NullChar = '\0';
+			foreach (var Char in Unescaped) {
+				CharPair [0] = Char;
+				CharPair [1] = NullChar;
+				EscapeChar (ref CharPair);
+				Escaped += CharPair [0];
+				if (CharPair [1] != NullChar)
+					Escaped += CharPair [1];
+			}
+
+			if (WrapInQuotes)
+				Escaped += "\"";
+			return Escaped;				
+		}
+
+		static public string EscapeValue(bool Value)
+		{
+			return Value ? "true" : "false";
+		}
+
 		static public void	Replace(ref string Json,string Key,string Value)
 		{
 			var StringElementPattern = GetNamePattern (Key) + PopX.Json.SemiColonPattern + ValueStringPattern;
 			var RegExpression = new Regex (StringElementPattern);
 
 			//	todo: json escaping!
-			var Replacement = '"' + Key + '"' + ':' + '"' + Value + '"';
+			var Replacement = '"' + Key + '"' + ':' + EscapeValue(Value);
 
 			//	harder to debug, but simpler implementation
 			Json = RegExpression.Replace (Json, Replacement);
 		}
 
+		static bool IsJsonWhitespace(char Char)
+		{
+			switch(Char)
+			{
+			case '\n':
+			case ' ':
+			case '\r':
+			case '\t':
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		static public void	Append(ref string Json,string Key,string Value)
+		{
+			//	construct what we're injecting
+			var NewContent = '"' + Key + '"' + ':' + EscapeValue(Value);
+
+			//	find end brace
+			var LastBrace = Json.LastIndexOf('}');
+			if (LastBrace < 0)
+				throw new System.Exception ("Missing end brace of json");
+
+			//	look for any json content between end of start brace
+			//	need to cope with comments
+			var HasJsonContent = false;
+			for (int i = LastBrace - 1;	i >= 0;	i--) {
+				var Char = Json [i];
+				if (IsJsonWhitespace (Char))
+					continue;
+				if (Char == '{')
+					break;
+				HasJsonContent = true;
+			}
+
+			if (HasJsonContent)
+				NewContent = ",\n" + NewContent;
+			NewContent += '\n';
+
+			Json = Json.Insert (LastBrace, NewContent);
+		}
 	}
 
 
