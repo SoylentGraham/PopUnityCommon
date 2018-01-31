a9c8bf1599f9c55e835c98d21a03681ffff648f2
Graham Reeves <graham@newchromantics.com>
2018-01-31 15:23:40 +0000

    Quick assert-style exception throw to avoid if()'s

---
+++ PopExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace PopX
+{
+	public static class Exception
+	{
+		public static void ThrowIf(bool Condition, string Error)
+		{
+			if (Condition)
+				throw new System.Exception(Error);
+		}
+
+		public static void Assert(bool Condition, string Error)
+		{
+			ThrowIf(!Condition, Error);
+		}
+	}
+}
+
