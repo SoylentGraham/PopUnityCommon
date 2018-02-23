fc3c74aa2a5975282c0b74f709a29d7a6d2d27b7
Graham Reeves <graham@grahamreeves.com>
2018-02-23 15:02:14 +0000

    Adding simple PopX.IO.SaveFile call to do all the file-saving stuff and callback a lambda

--- PopIO.cs
+++ PopIO.cs
@@ -8,11 +8,20 @@ namespace PopX
 {
 	public static class IO
 	{
-		static public string	Application_ProjectPath	{	get { return Application.dataPath.Split (new string[]{ "/Assets" }, System.StringSplitOptions.None) [0]; } }
-
-		public static void WriteStringToFile (string Filename, string Data)
+		//	file types we can write/export
+		//	gr: value used as extension (todo; support multiple extensions per type, eg. jpeg and jpg)
+		public enum ImageFileType
 		{
-			var FileHandle = System.IO.File.CreateText( Filename );
+			png,
+			jpg,
+			exr
+		};
+
+		static public string Application_ProjectPath { get { return Application.dataPath.Split(new string[] { "/Assets" }, System.StringSplitOptions.None)[0]; } }
+
+		public static void WriteStringToFile(string Filename, string Data)
+		{
+			var FileHandle = System.IO.File.CreateText(Filename);
 			FileHandle.Write(Data);
 			FileHandle.Close();
 		}
@@ -24,18 +33,42 @@ namespace PopX
 			//	gr: this seems to always return an empty string...
 			//UnityEditor.FileUtil.GetProjectRelativePath (Path);
 
-			if (string.IsNullOrEmpty (Path))
+			if (string.IsNullOrEmpty(Path))
 				return Path;
 
 			var ProjectPath = Application_ProjectPath;
 
-			if (!Path.StartsWith (ProjectPath))
-				throw new System.Exception ("Path " + Path + " is not project relative (" + ProjectPath + ")");
+			if (!Path.StartsWith(ProjectPath))
+				throw new System.Exception("Path " + Path + " is not project relative (" + ProjectPath + ")");
 
-			Path = Path.Remove (0, ProjectPath.Length);
+			Path = Path.Remove(0, ProjectPath.Length);
 			return Path;
 		}
 
+#if UNITY_EDITOR
+		public static void SaveFile(string DefaultName, ImageFileType Extension, System.Action<string, ImageFileType> DoSave)
+		{
+			var ExtensionsString = Extension.ToString();
+			var DefaultDirectory = Application.dataPath;
+
+			var Filename = UnityEditor.EditorUtility.SaveFilePanel("Save as...", DefaultDirectory, DefaultName, ExtensionsString);
+			if (string.IsNullOrEmpty(Filename))
+				throw new System.Exception("Save cancelled");
+
+			/*	gr: might still want this on windows if the user can choose a different extension. osx won't allow it
+			//	get the full path
+			//	work out the extension used
+			ImageFileType? Extension = null;
+			foreach (var Ext in Extensions)
+				if (Filename.EndsWith(Ext.ToString()))
+					Extension = Ext;
+			if (!Extension.HasValue)
+				throw new System.Exception("Couldn't determine file extension selected by user");
+			*/
+
+			DoSave(Filename, Extension);
+		}
+		#endif
 	}
 }
 
