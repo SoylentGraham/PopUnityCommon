7aa2fa174bb82c19fb6d821e2a05eb38c2532c31
Graham Reeves <graham@grahamreeves.com>
2018-03-08 23:35:40 +0000

    Added initial code to enumerate properties in a shader

---
+++ Editor/PopMaterial_Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+
+
+namespace PopX
+{
+	public class Material_UnitTest
+	{
+		[Test]
+		public void FindProperties()
+		{
+			var Shader = @"Shader ShaderName
+			{
+				Properties
+				{
+					Colour(""Colour"", COLOR) = (1, 1, 1, 1)
+					TextureVar( ""SomeTexture"" , 2D ) = ""white""
+				}
+				SubShader
+				{	
+				}
+			}";
+
+			var Props = PopX.Materials.GetProperties(Shader);
+			Assert.AreEqual(Props["Colour"], typeof(Color));
+			Assert.AreEqual(Props["TextureVar"], typeof(Texture));
+		
+		}
+
+	}
+}
+
