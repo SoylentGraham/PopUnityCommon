4de048b59bcadda40fcde5c985536eff1818c86f
Graham Reeves <graham@grahamreeves.com>
2018-03-14 14:12:10 +0000

    Removed object restriction, because I'm an idiot.

--- PopPlayerPrefs.cs
+++ PopPlayerPrefs.cs
@@ -6,17 +6,17 @@ public static class PopPlayerPrefs
 {
 	const string NullValue = "<null>";  //	if we serialise null, we write this. If we deseralise it instead of json, return null
 
-	public static void SetObject<T>(string Key, T Value) where T : Object
+	public static void SetObject<T>(string Key, T Value)
 	{
 		var Json = (Value == null) ? NullValue : JsonUtility.ToJson(Value);
 		PlayerPrefs.SetString(Key, Json);
 	}
 
-	public static T GetObject<T>(string Key) where T : Object
+	public static T GetObject<T>(string Key)
 	{
 		var Json = PlayerPrefs.GetString(Key);
 		if (Json.Equals(NullValue))
-			return (T)null;
+			return default(T);
 
 		var Obj = JsonUtility.FromJson<T>(Json);
 		return Obj;
