a3e79a8bb80ebfdcbcaca3d4551e28ae339b9427
Graham Reeves <graham@grahamreeves.com>
2018-03-22 11:06:45 +0000

    Added AssetDatabase extension (not actually an extension) to get all assets of a certain type

---
+++ PopAssetDatabase.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public static class PopAssetDatabase
+{
+	public static string GetTypeAssetTypeName(System.Type Type)
+	{
+		//	some special cases
+		if (Type == typeof(SceneAsset))
+			return "scene";
+		
+		return Type.Name;
+	}
+	
+	//	gr: maybe should allow this in production, but find assets from somewhere else? resources?
+#if UNITY_EDITOR
+	public static List<T> GetAssets<T>(string Filter=null) where T : UnityEngine.Object
+	{
+		var Assets = new List<T>();
+
+		//	get all guids
+		var Typename = GetTypeAssetTypeName(typeof(T));
+		var Guids = AssetDatabase.FindAssets("t:" + Typename + " " + Filter );
+		foreach ( var Guid in Guids )
+		{
+			var Path = AssetDatabase.GUIDToAssetPath(Guid);
+			var Asset = AssetDatabase.LoadAssetAtPath<T>(Path);
+			Assets.Add(Asset);
+		}
+
+		return Assets;
+	}
+#endif
+}
+
+
